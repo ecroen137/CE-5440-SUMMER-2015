2a743ba50d161dc6745c1ec5c2df5f695e006a52
Ed Hertzog <edhertzog@gmail.com>
2015-06-25 00:20:54 +0000

    push

--- class-4/js/mail.js
+++ class-4/js/mail.js
@@ -4,6 +4,17 @@ $(function() {
         if(!$(".validator-error").length) {
             var user = [$("#firstName").val(), $("#lastName").val()];
             console.log(JSON.stringify(user));
+            $.ajax(
+                {
+                    type: "POST",
+                    url: "https://ce-5440-summer-2015-01-phillywebguy.c9.io/class-4/validator-exercise.html",
+                    dataType: 'json',
+                    data: JSON.stringify(user),
+                    success: function() {
+                        console.log('complete');
+                    }
+                }
+                );
         }
     });
 });
